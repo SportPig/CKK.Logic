804acca83886f25f2a96ce0efa1ba9111c21dbe5
Bryan Williams <bryan.williams@hrblock.com>
2022-06-08 17:49:32 -0600

    Latest Changes 6_8_2022

--- CKK.Logic/Models/Store.cs
+++ CKK.Logic/Models/Store.cs
@@ -11,17 +11,7 @@ namespace CKK.Logic.Models
         private Product _product1;
         private Product _product2;
         private Product _product3;
-        
 
-        //public Store(int id, string name, Product product1, Product product2, Product product3)
-        //{
-       //     this.id = id;
-           // /_name = name;
-            //_product1 = product1;
-            //_product2 = product2;
-            // = product3;
-           
-       // }
         public int GetId()
         {
             return id;
